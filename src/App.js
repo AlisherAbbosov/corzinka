1fe845c0acbff783a4015f0345b4c24c8a135aa7
AlisherAbbosov <abbosov.2004@gmail.com>
2023-04-28 18:20:56 +0500

    Service page built

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
 
 function App() {
-  return <div className="App container">hello</div>;
+  return (
+    <div className="App container">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/Service/Service.jsx
@@ -0,0 +1,15 @@
+import "./Service.scss";
+
+import ServiceContent from "../../Components/ServiceContent/ServiceContent";
+
+function Service() {
+  return (
+    <div className="service">
+      <ServiceContent />
+      <ServiceContent />
+      <ServiceContent />
+    </div>
+  );
+}
+
+export default Service;
